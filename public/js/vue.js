4d18ab13184ad94b60b747fbe8490b4d49f87d64
mkataoka73 <mkataoka73@gmail.com>
2022-02-11 12:34:54 +0900

    Add Vue.js, separate public folders

---
+++ public/js/vue.js
@@ -0,0 +1,19 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+
+
+let table = new Vue({
+  el: '#table',
+  data: {
+    lessons: [
+      { no: 1, title: 'The Setup', done: false },
+      { no: 2, title: 'An Important Message', done: true },
+      { no: 3, title: 'More Printing', done: false },
+    ]
+  }
+})
